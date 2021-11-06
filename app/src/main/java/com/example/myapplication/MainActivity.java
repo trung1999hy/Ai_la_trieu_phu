a73ebabe630cb7897b8b1f499c142cca46b1530a
trung1999hy <trantrung1999hy@gmail.com>
2021-11-06 09:51:47 +0700

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.myapplication;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapplication.bxh.bxh;
+import com.example.myapplication.bxh.mocchoi;
+
+
+public class MainActivity extends AppCompatActivity {
+    private MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        playSound(R.raw.nenaltp);
+
+        khaibao();
+    }
+
+    public void khaibao() {
+        final Button batdau = (Button) findViewById(R.id.batDau);
+        final Button bx= (Button)findViewById(R.id.Bxh);
+        ImageView imageView = (ImageView) findViewById(R.id.icon);
+        imageView.startAnimation(AnimationUtils.loadAnimation(this,R.anim.xoay2));
+        Button thoat = (Button) findViewById(R.id.Exit);
+
+        /////////////////////////////////////////////////////////////////////////////////////
+        batdau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+                              Intent it = new Intent(MainActivity.this, mocchoi.class);
+                startActivity(it);
+
+            }
+        });
+
+        bx.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, bxh.class);
+                startActivity(it);
+            }
+        });
+        thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this,android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar_MinWidth);
+                builder.setTitle("Bạn có muốn thoát game!");
+                builder.setPositiveButton("Thoát", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent startMain = new Intent(Intent.ACTION_MAIN);
+                        startMain.addCategory(Intent.CATEGORY_HOME);
+                        startActivity(startMain);
+                        mediaPlayer.pause();
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.show();
+            }
+        });
+    }
+
+
+    public void playSound(int type) {
+        mediaPlayer = MediaPlayer.create(this, type);
+        mediaPlayer.start();
+    }
+}
